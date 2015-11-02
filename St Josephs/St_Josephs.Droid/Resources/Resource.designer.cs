eba35e865515b73088217819c214f54b37b37328
Brandon Andre <dev.brandonandre@gmail.com>
2015-11-02 17:08:54 -0500

    Added style.xml file and color.xml file.

--- St Josephs/St_Josephs.Droid/Resources/Resource.designer.cs
+++ St Josephs/St_Josephs.Droid/Resources/Resource.designer.cs
@@ -57,22 +57,6 @@ namespace St_Josephs.Droid
 			}
 		}
 		
-		public partial class Id
-		{
-			
-			// aapt resource value: 0x7f050000
-			public const int checkBox1 = 2131034112;
-			
-			static Id()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Id()
-			{
-			}
-		}
-		
 		public partial class Layout
 		{
 			
