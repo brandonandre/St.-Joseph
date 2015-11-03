d8cdb86fb7cd7453c0b7077c0db63e32e6c2f46b
Brandon Andre <brandon@lightstudios.ca>
2015-11-03 15:26:34 -0500

    Added CardView Package to android application.

--- St Josephs/St_Josephs.Droid/Resources/Resource.designer.cs
+++ St Josephs/St_Josephs.Droid/Resources/Resource.designer.cs
@@ -31,6 +31,39 @@ namespace St_Josephs.Droid
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010000
+			public const int cardBackgroundColor = 2130771968;
+			
+			// aapt resource value: 0x7f010001
+			public const int cardCornerRadius = 2130771969;
+			
+			// aapt resource value: 0x7f010002
+			public const int cardElevation = 2130771970;
+			
+			// aapt resource value: 0x7f010003
+			public const int cardMaxElevation = 2130771971;
+			
+			// aapt resource value: 0x7f010005
+			public const int cardPreventCornerOverlap = 2130771973;
+			
+			// aapt resource value: 0x7f010004
+			public const int cardUseCompatPadding = 2130771972;
+			
+			// aapt resource value: 0x7f010006
+			public const int contentPadding = 2130771974;
+			
+			// aapt resource value: 0x7f01000a
+			public const int contentPaddingBottom = 2130771978;
+			
+			// aapt resource value: 0x7f010007
+			public const int contentPaddingLeft = 2130771975;
+			
+			// aapt resource value: 0x7f010008
+			public const int contentPaddingRight = 2130771976;
+			
+			// aapt resource value: 0x7f010009
+			public const int contentPaddingTop = 2130771977;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -45,7 +78,19 @@ namespace St_Josephs.Droid
 		{
 			
 			// aapt resource value: 0x7f040000
-			public const int color_action_bar = 2130968576;
+			public const int cardview_dark_background = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int cardview_light_background = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int cardview_shadow_end_color = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int cardview_shadow_start_color = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int color_action_bar = 2130968580;
 			
 			static Color()
 			{
@@ -53,6 +98,28 @@ namespace St_Josephs.Droid
 			}
 			
 			private Color()
+			{
+			}
+		}
+		
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int cardview_compat_inset_shadow = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int cardview_default_elevation = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int cardview_default_radius = 2131034114;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
 			{
 			}
 		}
@@ -92,11 +159,11 @@ namespace St_Josephs.Droid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int app_name = 2131034113;
+			// aapt resource value: 0x7f070001
+			public const int app_name = 2131165185;
 			
-			// aapt resource value: 0x7f050000
-			public const int hello = 2131034112;
+			// aapt resource value: 0x7f070000
+			public const int hello = 2131165184;
 			
 			static String()
 			{
@@ -111,14 +178,23 @@ namespace St_Josephs.Droid
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f060001
-			public const int ActionBarTheme = 2131099649;
+			// aapt resource value: 0x7f060004
+			public const int ActionBarTheme = 2131099652;
+			
+			// aapt resource value: 0x7f060003
+			public const int AppTheme = 2131099651;
 			
 			// aapt resource value: 0x7f060000
-			public const int AppTheme = 2131099648;
+			public const int CardView = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int CardView_Dark = 2131099649;
 			
 			// aapt resource value: 0x7f060002
-			public const int MyTextAppearance = 2131099650;
+			public const int CardView_Light = 2131099650;
+			
+			// aapt resource value: 0x7f060005
+			public const int MyTextAppearance = 2131099653;
 			
 			static Style()
 			{
@@ -126,6 +202,65 @@ namespace St_Josephs.Droid
 			}
 			
 			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] CardView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971,
+					2130771972,
+					2130771973,
+					2130771974,
+					2130771975,
+					2130771976,
+					2130771977,
+					2130771978};
+			
+			// aapt resource value: 0
+			public const int CardView_cardBackgroundColor = 0;
+			
+			// aapt resource value: 1
+			public const int CardView_cardCornerRadius = 1;
+			
+			// aapt resource value: 2
+			public const int CardView_cardElevation = 2;
+			
+			// aapt resource value: 3
+			public const int CardView_cardMaxElevation = 3;
+			
+			// aapt resource value: 5
+			public const int CardView_cardPreventCornerOverlap = 5;
+			
+			// aapt resource value: 4
+			public const int CardView_cardUseCompatPadding = 4;
+			
+			// aapt resource value: 6
+			public const int CardView_contentPadding = 6;
+			
+			// aapt resource value: 10
+			public const int CardView_contentPaddingBottom = 10;
+			
+			// aapt resource value: 7
+			public const int CardView_contentPaddingLeft = 7;
+			
+			// aapt resource value: 8
+			public const int CardView_contentPaddingRight = 8;
+			
+			// aapt resource value: 9
+			public const int CardView_contentPaddingTop = 9;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
