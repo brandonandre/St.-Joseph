26b2b86c60f7f03342075af649c81fdff89a2283
Brandon Andre <brandon@lightstudios.ca>
2015-11-11 13:57:45 -0500

    Added the API Key for google maps

--- St Josephs/St_Josephs.Droid/Properties/AssemblyInfo.cs
+++ St Josephs/St_Josephs.Droid/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.0")]
+[assembly: AssemblyVersion ("0.0.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -27,4 +27,4 @@ using Android.App;
 //[assembly: AssemblyKeyFile("")]
 
 [assembly: UsesFeature(GLESVersion = 0x00020000, Required = true)]
-[assembly: MetaData("com.google.android.maps.v2.API_KEY", Value = "AIzaSyCe9FlFLkAo34iMnxnR23X3g_irRh7ir5E")]
+[assembly: MetaData("com.google.android.maps.v2.API_KEY", Value = "AIzaSyDL1QDPeT1UM537JhHxAZAkLuLpNSkSttA ")]
